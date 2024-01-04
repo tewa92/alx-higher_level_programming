8252451117d798948404a27655ccd930ec75de8c
tewa92 <tewachewsaleigizer@gmail.com>
2024-01-04 12:22:31 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
-    from hidden_4 import *
-    allf = dir():
-    for i in range (0, len(allf)):
-        if allf [i][:2] != "__":
-            print("{:s}".format(allf[i]))
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
