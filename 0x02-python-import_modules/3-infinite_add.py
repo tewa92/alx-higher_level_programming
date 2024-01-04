dd8f940b957017d4d71d850c5b1f694021900e8a
tewa92 <tewachewsaleigizer@gmail.com>
2024-01-04 12:21:04 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
-    from sys import argv
-    sumit = 0
-    for i in range(1, len(argv)):
-        sumit += int(argv[i])
-    print("{}".format(sumit))
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
