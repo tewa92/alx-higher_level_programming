f591d6aa63f8fb96ff2915ea27af8fa09e2c3933
Tewachew Saleigizer <123402812+tewa92@users.noreply.github.com>
2024-01-30 10:42:43 +0300

    101-lazy_matrix_mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+ paraphrase the comment
+ """
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """This function computes the matrix product of two matrices."""
+    return numpy.matmul(m_a, m_b)
