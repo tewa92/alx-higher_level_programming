d0c45f3a9bdb615100669057d83224af779be8c8
tewachewsaleigizer@gmail.com <tewachewsaleigizer@gmail.com>
2024-02-06 00:23:09 -0800

    python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ This module includes the function inherits_from.
 def inherits_from(obj, a_class):
     """
     This function returns true if 'obj' is a subclass of 'a_class';
-    otherwise, it returns false.
+        otherwise, it returns false.
     """
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
