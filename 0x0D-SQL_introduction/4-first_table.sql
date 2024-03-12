48a465dbd7618c859283155adcaed491c96eb818
tewa92 <tewachewsaleigizer@gmail.com>
2024-03-12 12:10:54 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- creates a tabel first_table in current db
--- does not fail if table exists
+-- script that creates a tabel first_table in current db
+-- if table exists script should not fail
 CREATE TABLE IF NOT EXISTS first_table (id INT,
 name VARCHAR(256));
