53c8b9f7e5c93db5966eadb6979f9af1a993dda2
Tewachew Saleigizer <123402812+tewa92@users.noreply.github.com>
2024-04-10 15:07:59 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-
-const farg = fs.readFileSync(process.argv[2]).toString();
-const sarg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], farg + sarg);
