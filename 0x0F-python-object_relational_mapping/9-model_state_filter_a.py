31556e47286f2f61250b0483309de0616f8ee2ac
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-14 06:55:37 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Print the state name from db hbtn_0e_6_usa if they contain letter 'a' inside the name
+Print the state name from db hbtn_0e_6_usa if they
+contain letter 'a' inside the name
 """
 
 import sys
@@ -10,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
