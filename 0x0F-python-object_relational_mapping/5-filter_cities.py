bdf9fcd896f82a600e1edd86ad4059a64eddbb8d
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-14 03:48:06 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,12 +9,15 @@ if __name__ == "__main__":
     """
     Access to the db get list of the states from db
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], poet=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], poet=3306)
     cur = db.cursor()
-    cur.excute(""" SELECT cities.name FROM cities INNER JOIN states ON states.id=cities.states_id WHERE states.name=%s""", (sys.argv[4],))
+    cur.excute(""" SELECT cities.name FROM cities
+                INNER JOIN states ON states.id=cities.states_id
+                WHERE states.name=%s""", (sys.argv[4],))
 
-rows = cur.fetchall()
-tmp = list(row[0] for row in rows)
-print(*tmp, sep=", ")
-cur.close()
-db.close()
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
