96b9b8c2e84c6d5f00fd7feb92557a5bfc11d5c1
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-14 02:30:16 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur.execute("""SELECT * FROM states
                  WHERE name LIKE BINARY 'N%'
                  ORDER BY states.id""")
-    rows = cur.fetch()
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
