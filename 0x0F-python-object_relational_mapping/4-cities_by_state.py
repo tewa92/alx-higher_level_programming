a221b2b3783abf62a224cbe2f00bb3c8bbf18a1b
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-14 02:49:29 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
     """
     Access to the db get list of the states from db
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.excute(""" SELECT cities.id, cities.name, states.id=cities.states_id """)
+    cur.execute(""" SELECT cities.id,
+                cities.name,
+                states.id=cities.states_id """)
     rows = cur.fetchall()
     for row in rows:
         print(row)
